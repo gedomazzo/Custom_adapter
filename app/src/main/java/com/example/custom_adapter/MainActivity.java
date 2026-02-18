7e50eed65c52a6f168f0ca61f5011b189b9e3536
Vasya <br2876@bs.amalnet.k12.il>
2026-02-18 14:04:45 +0200

    adding the description to each costume

--- app/src/main/java/com/example/custom_adapter/MainActivity.java
+++ app/src/main/java/com/example/custom_adapter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.custom_adapter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     String[] cate = {"super heroes", "worker", "animals"};
     Costumes[] costumes = {
             new Costumes("cat girl", R.drawable.cat_woman, 3, cate[0]),
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             new Costumes("doctor", R.drawable.doctor, 4, cate[1]),};
 
     Spinner spinner;
+    TextView desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +35,36 @@ public class MainActivity extends AppCompatActivity {
 
         CostumeAdapter costumeAdapter = new CostumeAdapter(this, costumes);
 
+        desc = (TextView) findViewById(R.id.desc);
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(costumeAdapter);
+        spinner.setOnItemSelectedListener(this);
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position == 0) {
+            desc.setText("A sleek, black catsuit costume, often including a cat-ear cowl and a whip. It's designed for stealth and agility.");
+        } else if (position == 1) {
+            desc.setText("A fun and fearsome shark costume, typically a one-piece suit with a fin on the back and a hood with felt teeth.");
+        } else if (position == 2) {
+            desc.setText("The classic blue and red Superman suit, featuring the iconic 'S' shield on the chest and a flowing red cape.");
+        } else if (position == 3) {
+            desc.setText("A detailed red and blue suit with a web pattern. This costume includes a full face mask with expressive eyes.");
+        } else if (position == 4) {
+            desc.setText("A warm and fuzzy full-body bear suit. This comfortable costume is made of soft, brown faux fur and includes a hood with bear ears.");
+        } else if (position == 5) {
+            desc.setText("A rugged woodworker outfit, usually consisting of a plaid flannel shirt, a tool belt with plastic tools, and a hard hat.");
+        } else if (position == 6) {
+            desc.setText("A professional-looking doctor's costume, featuring a white lab coat, a toy stethoscope, and sometimes a surgical mask.");
+        }
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    
+    }
+
 }
